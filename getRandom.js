fa8fc87578debd246b938ea27f92ac315d028911
prakin77 <58230098+prakin77@users.noreply.github.com>
2020-02-06 16:31:35 -0500

    computerWin file is added but still has an error message

--- getRandom.js
+++ getRandom.js
@@ -6,3 +6,27 @@ function getRandom(min, max) {
   }
   
   console.log(getRandom(0, 100))
+
+  const readline = require ('readline')
+
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+
+function ask(question){
+    return new Promise(function(resolve, reject){
+        readlineInterface.question(question, resolve)
+    })
+}
+
+async function hello(){
+  //if name is Darth Voldemort Lex Luthor "NOOOO! That's impossible!"
+  //else say "Hello!"
+  let name = await ask('What is your name?')
+  if (name==="Darth" || name==="Voldemort" || name==="Lex Luthor"){
+      console.log("NOOO! That's impossible!")  
+  } else {
+      console.log("Hello! " + capitalize(name) + "!")  
+  }
+  process.exit()
+}
+
+hello()
