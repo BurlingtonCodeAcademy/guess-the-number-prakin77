ccd1c4372d8100db9a73472233ae5a4c7af4c82e
prakin77 <58230098+prakin77@users.noreply.github.com>
2020-02-06 15:59:07 -0500

    initial commit

--- index.js
+++ index.js
@@ -14,5 +14,15 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+  
+  console.log(getRandomInt(0, 100))
+
   process.exit();
 }
+
